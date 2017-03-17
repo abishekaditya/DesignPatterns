3cb11e0542a4348c3d73e660be4df8485338c56a
Abishek Aditya <abishekaditya@outlook.com>
2017-03-17 17:03:27 +0530

    Stratergy Pattern Done

---
+++ Ducks/FlyNope.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ducks
+{
+    class FlyNope : IFlyBehaviour
+    {
+        public void fly()
+        {
+            Console.WriteLine("I can't fly");
+        }
+    }
+}
--- Ducks/Program.cs
+++ Ducks/Program.cs
@@ -3,13 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ducks;
 
 namespace Ducks
 {
+    class Duck
+    {
+        private IQuackBehaviour _quacker;
+        private IFlyBehaviour _flyer;
+
+
+        public IQuackBehaviour Quacker {
+            set
+            {
+                this._quacker = value;
+            }
+        }
+
+        public IFlyBehaviour Flyer
+        {
+            set
+            {
+                this._flyer = value;
+            }
+        }
+
+
+
+        public void PerformQuack()
+        {
+            _quacker.quack();
+        }
+
+        public void PerformFly()
+        {
+            _flyer.fly();
+        }
+    }
+
+    class MallardDuck : Duck
+    {
+        public MallardDuck()
+        {
+            Flyer = new FlyNope();
+            Quacker = new QuackNope();
+        }
+
+        public void Display()
+        {
+            this.PerformFly();
+            this.PerformQuack();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var mallard = new MallardDuck();
+            mallard.Quacker = new QuackNormal();
+            mallard.Display();
+            mallard.Flyer = new FlyWings();
+            mallard.Display();
+
         }
     }
 }
