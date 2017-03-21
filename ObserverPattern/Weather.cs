6aba873126dccdc3114ff44905389fc5ee9e56ad
Abishek Aditya <abishekaditya@outlook.com>
2017-03-22 01:44:18 +0530

    A

---
+++ ObserverPattern/Weather.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    class Weather
+    {
+        double temp;
+        double humd;
+        double pres;
+
+        public double Pressure
+        {
+            get { return this.pres; }
+        }
+
+        public double Humidity
+        {
+            get { return this.humd; }
+        }
+
+        public double Temperature
+        {
+            get { return this.temp; }
+        }
+
+        public Weather(double humd, double pres, double temp)
+        {
+            this.temp = temp;
+            this.pres = pres;
+            this.humd = humd;
+        }
+    }
+}
---
+++ ObserverPattern/WeatherMonitor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    class WeatherMonitor : IObserver<Weather>
+    {
+        private IDisposable cancellation;
+        private string name;
+
+        public void Subscribe(WeatherSupplier provider)
+        {
+            cancellation = provider.Subscribe(this);
+        }
+
+        public virtual void Unsubscribe()
+        {
+            cancellation.Dispose();
+        }
+
+        public WeatherMonitor(string name)
+        {
+            this.name = name;
+        }
+        
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("Error has occured");
+        }
+
+        public void OnNext(Weather value)
+        {
+            Console.WriteLine(this.name);
+            if (name.Contains("T"))
+            {
+                string op = $"| Temperature : {value.Temperature} Celsius |";
+                Console.Write(op);
+                
+            }
+            if (name.Contains("P"))
+            {
+                string op = $"| Pressure : {value.Pressure} atm |";
+                Console.Write(op);
+            }
+            if (name.Contains("H"))
+            {
+                string op = $"| Humidity : {value.Humidity*100} % |";
+                Console.Write(op);
+            }
+            if (!(name.Contains("T") || name.Contains("P") || name.Contains("H")))
+            {
+                this.OnError(new Exception());
+            }
+            Console.WriteLine();
+        }
+    }
+}
