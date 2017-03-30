c340b753d7d184d2b2fbcdfc64091143fa2faa97
Abishek Aditya <abishekaditya@outlook.com>
2017-03-30 19:16:01 +0530

    Template Pattern

---
+++ TemplatePattern/Beverages/Beverage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TemplatePattern
+{
+    public abstract class Beverage
+    {
+        protected int _sugar;
+        public void Prepare()
+        {
+            Boil();
+            Brew();
+            Pour();
+            if (WantsCondiments)
+                AddCondiments();
+
+        }
+
+        public bool WantsCondiments { private get; set; }
+
+
+        protected abstract void Brew();
+
+        private void Boil()
+        {
+            Console.WriteLine("Boling Water");
+        }
+
+        private void Pour()
+        {
+            Console.WriteLine("Pouring in Cup");
+        }
+
+        protected abstract void AddCondiments();
+
+        public virtual int AddSugar { get; set; }
+
+        protected void Sugar() { }
+    }
+}
---
+++ TemplatePattern/Beverages/Tea.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TemplatePattern
+{
+    class Tea : Beverage
+    {
+        protected override void Brew()
+        {
+            Console.WriteLine("Adding tea leaves to water and boil");
+        }
+
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("Adding Lemon and Sugar");
+            Sugar();
+        }
+
+        private void Sugar()
+        {
+            Console.WriteLine($"adding {_sugar} spoons of sugar");
+        }
+
+        public override int AddSugar
+        {
+            set { _sugar = value; }
+        }
+    }
+}
