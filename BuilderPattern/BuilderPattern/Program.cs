abd12be1387418660e1bda9e45bf7de446ba346b
basbros <basbros@libero.it>
2019-05-20 14:38:49 +0200

    Add builder pattern (#14)
    * Add builder pattern
    
    * Revert VS2019 info from .sln file
    
    * Downgrade target framework to core 2.0
    
    * Fix GUID of BuildPattern project type

---
+++ BuilderPattern/BuilderPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BuilderPattern
+{
+    class Program
+    {
+        static void Main()
+        {
+            var myHamburger = new HamburgerBuilder()
+                .AddBread()
+                .AddMeal()
+                .AddCheese()
+                .AddTomato()
+                .AddSalad()
+                .AddMayonnaise()
+                .Build();
+            Console.WriteLine($"My hamburger: {myHamburger}");
+
+            var myWifeHamburger = new HamburgerBuilder()
+                .AddBread()
+                .AddSalad()
+                .Build();
+            Console.WriteLine($"My wife's hamburger: {myWifeHamburger}");
+        }
+    }
+}
