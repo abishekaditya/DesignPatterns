87ee2d0dbfc24f48ace3bd9cb2147d3f59053eb3
Alena <Ale-na98@yandex.ru>
2022-10-07 17:18:50 +0200

    Add unit tests for Factory pattern

---
+++ FactoryPattern.Tests/FactoryMethodTests.cs
@@ -0,0 +1,36 @@
+﻿namespace FactoryPattern.Tests
+{
+    [TestFixture]
+    public class FactoryMethodTests
+    {
+        [TestCase(typeof(ClamPizza), "Clam")]
+        [TestCase(typeof(CheesePizza), "Cheese")]
+        [TestCase(typeof(VeggiePizza), "Veggie")]
+        public void Create_PizzaFromNyPizzaFactory_ReturnsPizzaOfCertainType(Type expected, string pizzaType)
+        {
+            // Arrange
+            var nyPizzaFactory = new NyPizzaFactory();
+
+            // Act
+            var actual = nyPizzaFactory.Order(pizzaType);
+
+            // Assert
+            Assert.That(actual, Is.InstanceOf(expected));
+        }
+
+        [TestCase(typeof(ClamPizza), "Clam")]
+        [TestCase(typeof(CheesePizza), "Cheese")]
+        [TestCase(typeof(VeggiePizza), "Veggie")]
+        public void Create_PizzaFromChicagoPizzaFactory_ReturnsPizzaOfCertainType(Type expected, string pizzaType)
+        {
+            // Arrange
+            var chicagoPizzaFactory = new ChicagoPizzaFactory();
+
+            // Act
+            var actual = chicagoPizzaFactory.Order(pizzaType);
+
+            // Assert
+            Assert.That(actual, Is.InstanceOf(expected));
+        }
+    }
+}
