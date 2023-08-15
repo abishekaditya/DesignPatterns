f181d53dcbe26aec3852feac5f9efff7c4df5fc2
Phí Minh Tiến <88813933+minhtien1101@users.noreply.github.com>
2023-08-15 20:51:59 +0700

    Update MallardDuck.cs
    u

--- AdapterPattern/MallardDuck.cs
+++ AdapterPattern/MallardDuck.cs
@@ -4,13 +4,13 @@ namespace AdapterPattern
 {
     class MallardDuck : IDuck
     {
-        // Quack
+        
         public void Quack()
         {
             Console.WriteLine("Quack Quack Quack");
         }
 
-        // Fly
+        
         public void Fly()
         {
             Console.WriteLine("Flies 500 Metres");
